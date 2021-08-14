545b021539be0dc6290ba016dff8492deb2987cc
Piyush Ahir <egpiyushahir@gmail.com>
2021-08-14 23:19:05 +0530

    feat - keyInSelect

--- index.js
+++ index.js
@@ -10,59 +10,68 @@ var hColor = chalk.hex('#DEADED');
 
 console.log(hColor("Let's see how well you know PIYUSH ❤️ \n"));
 var userName = read.question("Namaste \nPlease enter your name? ");
-console.log(hColor("\nWelcome, "+userName+", to Do You Know Piyush? "));
+console.log(hColor("\nWelcome, " + userName + ", to Do You Know Piyush? "));
 console.log(chalk.black.bold.bgWhite("\nNOTE: For each correct answer you will get 1 point.\n"));
 
 var highScores = [{
-  name : "Yuvraj",
-  scored : 3
+    name: "Yuvraj",
+    scored: 4
 }, {
-  name : "Ram",
-  scored : 2
+    name: "Ram",
+    scored: 4
 }];
 
 var score = 0;
 
-function checkHighScore(){
-  for(var i=0; i<highScores.length; i++){
-    if(score>highScores[i].scored){
-      console.log(chalk.bold.blueBright("\nYay! You beat the high scorer, please send me a screenshot of this so that I can update the list.\n"));
-      console.log("-------------------");
-      break;
+function checkHighScore() {
+    for (var i = 0; i < highScores.length; i++) {
+        if (score > highScores[i].scored) {
+            console.log(chalk.bold.blueBright("\nYay! You beat the high scorer, please send me a screenshot of this so that I can update the list of high scorer.\n"));
+            console.log("-------------------");
+            break;
+        }
     }
-  }
-}
-
-function askQuestions(ques, ans){
-  var userAns = read.question(ques);
-  if(userAns.toLowerCase() == ans.toLowerCase()){
-    console.log(rightAns("\nYay! You're right :)"));
-    score++;
-  }else{
-    console.log(wrongAns("\nOops! You're wrong :("));
-  }
-
-  console.log(sColor("Current Score : "+score));
-  console.log("-------------------");
 }
 
 var questions = [{
-  question : "\nWhat's My full Name?\n",
-  answer : "Piyush Ahir"
+    question: "\nWhat's My full Name?\n",
+    mcq: ['Piyush', 'Piyush Ahir', 'Piyush Rakesh'],
+    answer: "1"
 }, {
-  question : "\nWhere do I live?\n",
-  answer : "Neemuch"
+    question: "\nWhere do I live?\n",
+    mcq: ['Neemuch', 'Ujjain', 'Indore'],
+    answer: "0"
 }, {
-  question : "\nWho is my best friend?\n",
-  answer : "Shoaib"
+    question: "\nWho is my best friend?\n",
+    mcq: ['Ram', 'Dheeraj', 'Shoaib'],
+    answer: "2"
 }, {
-  question : "\nWhich phone I am using?\n",
-  answer : "MI A3"
+    question: "\nWhich phone I am using?\n",
+    mcq: ['iPhone 12 Mini', 'MI A3', 'Nokia 3310'],
+    answer: "1"
+}, {
+    question: "\nWhat's my college name?\n",
+    mcq: ['MIT, Ujjain', 'MIT, Pune', 'MIT, USA'],
+    answer: "0"
 }];
 
-for(var i=0; i<questions.length; i++){
-  var q = questions[i];
-  askQuestions(q.question, q.answer);
+for (var i = 0; i < questions.length; i++) {
+    console.log("Q" + (i + 1));
+    play(questions[i]);
+    console.log(sColor("Current Score : " + score));
+    console.log("-------------------");
+}
+
+function play({ question, mcq, answer }) {
+
+    const index = read.keyInSelect(mcq, question);
+
+    if (index == answer) {
+        console.log(rightAns("\nYay! You're right :)"));
+        score++;
+    } else {
+        console.log(wrongAns("\nOops! You're wrong :("));
+    }
 }
 
 console.log(chalk.bold.magentaBright("Yay! Your total score is : " + score));
